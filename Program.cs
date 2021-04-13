ab94ec31c29a556377bb72001ea3fb2b187bb4b6
Bianca Brown <biancakbrown58@gmail.com>
2021-04-13 13:34:20 -0400

    Finished with Explorer Mode

--- Program.cs
+++ Program.cs
@@ -2,11 +2,73 @@
 
 namespace VariablesCS
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            //Creating Variables
+            var numberOfCupsOfCoffee = 0;
+            var fullName = "Bianca Brown";
+            var today = DateTime.Now;
+            Console.WriteLine($"I drink this many cups of coffee a day: { numberOfCupsOfCoffee}. My name is: { fullName}. Today's Date is: { today}.");
+
+            //Getting Input From the User
+            Console.WriteLine("");
+            Console.Write("What is your name? ");
+            var userName = Console.ReadLine();
+            Console.WriteLine($"Hi there {userName}! Welcome!");
+
+            //Getting Different Types of Input From the User
+            Console.WriteLine("");
+            Console.Write($"{userName}, will you enter a number? ");
+            var firstNumberAsString = Console.ReadLine();
+            Console.Write($"Awesome, thank you! {userName}, will you enter another number? ");
+            var secondNumberAsString = Console.ReadLine();
+
+            //Converting String Input Into Numbers
+            var firstOperand = double.Parse(firstNumberAsString);
+            var secondOperand = double.Parse(secondNumberAsString);
+
+            //Doing Math
+            Console.WriteLine("");
+
+            //Results Message
+            Console.WriteLine("Here are some different results of your two numbers!");
+            Console.WriteLine("");
+
+            //Adding
+            Console.WriteLine("+Adding+");
+            Console.WriteLine("~~~~~~~~~");
+            var sum = firstOperand + secondOperand;
+            Console.WriteLine($"The total of your two numbers are: {sum}");
+            Console.WriteLine("");
+
+            //Subtracting
+            Console.WriteLine("-Subtracting-");
+            Console.WriteLine("~~~~~~~~~~~~~~");
+            var difference = secondOperand - firstOperand;
+            Console.WriteLine($"The difference of you two numbers are: {difference}");
+            Console.WriteLine("");
+
+            //Multiply
+            Console.WriteLine("*Multiplying*");
+            Console.WriteLine("~~~~~~~~~~~~~~~~");
+            var product = firstOperand * secondOperand;
+            Console.WriteLine($"Your two numbers multiplied are: {product}");
+            Console.WriteLine("");
+
+            //Divide
+            Console.WriteLine("/Dividing/");
+            Console.WriteLine("~~~~~~~~~~~~~~");
+            var quotient = firstOperand / secondOperand;
+            Console.WriteLine($"Your two numbers divied are: {quotient}");
+            Console.WriteLine("");
+
+            //Remainder
+            Console.WriteLine("%Remainder%");
+            Console.WriteLine("~~~~~~~~~~~~~~");
+            var remainder = firstOperand % secondOperand;
+            Console.WriteLine($"The remainder of the two divided numbers are: {remainder}");
+        }
     }
-  }
 }
